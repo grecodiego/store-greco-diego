30106eeb1353eedef4bf5484679d3410efeefa3b
grecodiego <greco.diego@gmail.com>
2021-09-21 13:26:44 -0300

    Correcciones Router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import {Results} from "../components/body/results/results"
 import {History} from "../components/body/history/history"
 
-export const Router = () => {
+export const Routes = () => {
 	return (
-        <Switch>
-            <Route exact path="store-greco-diego/" > 
-                <Results/>
-            </Route>
-            <Route path="store-greco-diego/history/" > 
-                <History/>
-            </Route>
-        </Switch>)
+        <Router>
+            <Switch>
+                <Route exact path="store-greco-diego/" > 
+                    <Results/>
+                </Route>
+                <Route path="store-greco-diego/history/" > 
+                    <History/>
+                </Route>
+            </Switch>
+        </Router>)
 }
 
 
